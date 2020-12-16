eb6baac423e5d29e9471b511a8ca3002c3fb3ba4
blangolas <daniel.zweng@gmx.net>
2020-12-16 20:25:34 +0100

    added alter attribute

--- src/at/htlwienwest/Person.java
+++ src/at/htlwienwest/Person.java
@@ -4,18 +4,20 @@ public class Person
 {
     private String vorname;
     private String nachname;
+    private int alter;
 
-    public Person(String vorname, String nachname)
+    public Person(String vorname, String nachname, int alter)
     {
-        this.vorname = vorname;
-        this.nachname = nachname;
+        this.vorname= vorname;
+        this.nachname= nachname;
+        this.alter= alter;
     }
 
     public String getVorname() { return vorname; }
     public String getNachname() { return nachname; }
 
-    public void setVorname(String vorname) { this.vorname = vorname; }
-    public void setNachname(String nachname) { this.nachname = nachname; }
+    public void setVorname(String vorname) { this.vorname= vorname; }
+    public void setNachname(String nachname) { this.nachname= nachname; }
 
     @Override
     public String toString() { return " Vorname: " +vorname+ ", Nachname: " +nachname+ "\n"; }
