23e2f0193df35f42839be7e9a4eca7b16fd82c02
Veton Staraveci <v.staraveci@austrian-standards.at>
2020-12-16 20:24:42 +0100

    add new attribute

--- src/at/htlwienwest/Person.java
+++ src/at/htlwienwest/Person.java
@@ -4,11 +4,13 @@ public class Person
 {
     private String vorname;
     private String nachname;
+    private int age;
 
-    public Person(String vorname, String nachname)
+    public Person(String vorname, String nachname, int age)
     {
         this.vorname = vorname;
         this.nachname = nachname;
+        this.age = age;
     }
 
     public String getVorname() { return vorname; }
@@ -18,5 +20,5 @@ public class Person
     public void setNachname(String nachname) { this.nachname = nachname; }
 
     @Override
-    public String toString() { return "Vorname: " +vorname+ ", Nachname: " +nachname; }
+    public String toString() { return "Vorname: " +vorname+ ", Nachname: " +nachname + "\n"; }
 }
