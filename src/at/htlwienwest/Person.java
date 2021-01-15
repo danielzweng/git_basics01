4dedda3513559976cab9fe4cb9cfb202a599d3ea
Veti27 <staraveci.v93@htlwienwest.at>
2021-01-15 16:12:23 +0100

    change geburtstag method from private to public

--- src/at/htlwienwest/Person.java
+++ src/at/htlwienwest/Person.java
@@ -41,7 +41,7 @@ public class Person
     public void setVorname(String vorname) { this.vorname = vorname; }
     public void setNachname(String nachname) { this.nachname = nachname; }
 
-    private void geburtstag()
+    public void geburtstag()
     {
         this.age++;
     }
